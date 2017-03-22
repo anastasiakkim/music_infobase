a1688d029d0aaf37312b47ca8c52bdce55f0ec50
Анастасия Ким <aakim_2@edu.hse.ru>
2017-03-23 01:05:24 +0300

    New class created

---
+++ Music_InfoBase/Musician.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_InfoBase
+{
+    class Musician
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _dateOfBirth;
+
+        public int DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        private string _country;
+
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+
+        private string _album;
+
+        public string Album
+        {
+            get { return _album; }
+            set { _album = value; }
+        }
+    }
+}
